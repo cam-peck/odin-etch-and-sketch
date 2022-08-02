6f14520d3d61590248f0f283508b01577acd0b48
Cameron <cameronpeck317@gmail.com>
2022-08-02 15:43:23 -0400

    finish event listeners for buttons and create slider

--- scripts.js
+++ scripts.js
@@ -1,8 +1,24 @@
 let numberOfSquares = 16;
-let pixelHeight = `${600 / numberOfSquares}px`
-let pixelWidth = `${600 / numberOfSquares}px`
+let pixelHeight = `${550 / numberOfSquares}px`
+let pixelWidth = `${550 / numberOfSquares}px`
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+const sliderText = document.querySelector('#slider-text')
 
 const drawContainer = document.querySelector('.drawing-container');
+const blackButton = document.querySelector('#black-btn')
+const rainbowButton = document.querySelector('#rainbow-btn')
+const chooseButton = document.querySelector('#colorpicker')
+const eraserButton = document.querySelector('#eraser-btn')
+const clearButton = document.querySelector('#clear-btn')
+
+chooseButton.addEventListener('change', watchColorPicker, true)
+
+function getRandomColor () {
+    let randomColorTag = Math.floor(Math.random()*16777215).toString(16);
+    return `#${randomColorTag}`
+}
 
 // create rows
 for (let i = 0; i <= numberOfSquares - 1; i++) {
@@ -33,3 +49,45 @@ pixels.forEach((pixel) => {
     pixel.style.width = pixelWidth;
 });
 
+blackButton.addEventListener('click', () => {
+    pixels.forEach((pixel) => {
+        // change color whne hovered over
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.backgroundColor = 'black';
+        });
+    });
+});
+
+rainbowButton.addEventListener('click', () => {
+    pixels.forEach((pixel) => {
+        // change color whne hovered over
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.backgroundColor = getRandomColor();
+        });
+    });
+});
+
+
+function watchColorPicker (event) {
+   let chosenColor = event.target.value
+   pixels.forEach((pixel) => {
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.backgroundColor = `${chosenColor}`;
+        });
+    });
+};
+
+eraserButton.addEventListener('click', () => {
+    pixels.forEach((pixel) => {
+        // change color whne hovered over
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.backgroundColor = 'white';
+        });
+    });
+});
+
+clearButton.addEventListener('click', () => {
+    pixels.forEach((pixel) => {
+        pixel.style.backgroundColor = 'white'
+    });
+});
