ecfc64fcb7eba45617b452e50c389abedc296266
Cameron <cameronpeck317@gmail.com>
2022-08-02 11:46:46 -0400

    add scaling to pixel container and base layout for index.html

--- scripts.js
+++ scripts.js
@@ -1,24 +1,35 @@
 let numberOfSquares = 16;
+let pixelHeight = `${600 / numberOfSquares}px`
+let pixelWidth = `${600 / numberOfSquares}px`
 
 const drawContainer = document.querySelector('.drawing-container');
-
 
 // create rows
 for (let i = 0; i <= numberOfSquares - 1; i++) {
     const drawColumn = document.createElement('div');
-    drawColumn.classList.add('pixel');
     drawColumn.classList.add('column');
     drawContainer.appendChild(drawColumn);
 }
 
+// grab all columns
 const gridColumn = document.querySelectorAll('.column')
-console.log(gridColumn)
-
 //create columns
 gridColumn.forEach((e) => {
     for (let i = 0; i <= numberOfSquares - 1; i++) {
         const drawRow = document.createElement('div');
-        drawRow.classList.add('pixel')
-        e.appendChild(drawRow)
-    }
+        drawRow.classList.add('pixel');
+        e.appendChild(drawRow);
+    };
 });
+
+const pixels = document.querySelectorAll('.pixel')
+pixels.forEach((pixel) => {
+    // change color whne hovered over
+    pixel.addEventListener('mouseover', () => {
+        pixel.style.backgroundColor = 'red';
+    });
+    // match size of input container
+    pixel.style.height = pixelHeight;
+    pixel.style.width = pixelWidth;
+});
+
